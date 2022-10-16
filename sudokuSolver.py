71155c5991a6f2f76d1022b5fe5660ee0b4933a3
Brian Wells <briswells@gmail.com>
2022-10-16 15:00:43 -0700

    code for base algorithm

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -38,7 +38,7 @@ def check_location(location):
     global args
     global possible_grid
     possible_values = []
-    for i in possible_grid[location[0]][location[1]]:
+    for i in range(1,args.s+1):
         if check_legal(i, location):
             possible_values.append(i)
     if len(possible_values) > 0:
@@ -46,49 +46,13 @@ def check_location(location):
     else:
         return False
 
-def generate_possible_grid():
-    global total_itrs
-    global grid
-    global possible_grid
-    total_itrs += 1
-    for y in range(len(grid)):
-        for x in range(len(grid[y])):
-            if grid[y][x] != 0:
-                possible_values = check_location((y,x))
-                if possible_values:
-                    possible_grid[y][x] = possible_values
-
-#Scans grid for cells that can only have one value before main algorithm
-def simple_cell_solver(window,canvas):
-    while True: #Loops until a simple value isn't found
-        global total_itrs
-        global grid
-        total_itrs += 1
-        found_simple = False
-        for y in range(len(grid)):
-            for x in range(len(grid[y])):
-                if grid[y][x] == 0:
-                    possible_values = check_location((y,x))
-                    if len(possible_values) == 1:
-                        found_simple = True
-                        grid[y][x] = possible_values[0]
-                        if animate:
-                            label = 'entry:{}{}'.format(y,x)
-                            number = canvas.create_text(35+50*x, 35+50*y, font=("Purisa",'30','bold'),
-                            text=grid[y][x],justify=CENTER, anchor=CENTER,fill='#3fba2f',
-                            tag=label)
-                            window.update()
-                            sleep(sleep_time)
-        if found_simple == False:
-            return
-
 #Recursive function that tries all possiblities until solution is found
-def brute_solver(window,canvas):
+def brute_solver():
     if not done:
         global total_itrs
-        total_itrs += 1
         global grid
         global last_value
+        total_itrs += 1
         for y in range(len(grid)): #loops through grid until blank cell is found
             for x in range(len(grid[y])):
                 if grid[y][x] == 0:
@@ -98,24 +62,10 @@ def brute_solver(window,canvas):
                     else:
                         for value in possible_values:
                             grid[y][x] = value #Places val into cell
-                            if animate:
-                                entry = grid[y][x]
-                                if entry > 9:
-                                    entry = chr( entry + 55)
-                                label = 'entry:{}{}'.format(y,x)
-                                number = canvas.create_text(35+50*x, 35+50*y, font=("Purisa",'30','bold'),
-                                text=entry,justify=CENTER, anchor=CENTER,fill='#d12828',
-                                tag=label)
-                                window.update()
-                                sleep(sleep_time)
-                            brute_solver(window,canvas) #recurs with updated grid
+                            brute_solver() #recurs with updated grid
                             if grid[last_value[0]][last_value[1]] != 0:
                                 return True #if grid is completed returns true
                             grid[y][x] = 0 #undo's grid update and continues to next possible val
-                            if animate:
-                                canvas.delete(number)
-                                window.update()
-                                sleep(sleep_time)
                         return True
 
 #Finds the last blank value in grid
@@ -125,30 +75,6 @@ def find_last(grid):
             if grid[y][x] == 0:
                 return (y,x)
     return True
-
-#Creates animation window with base values
-def make_base(window):
-    global grid
-    global args
-    canvas = Canvas(window, width=50*(args.s)+20, height=50*(args.s)+2, bd=0, highlightthickness=0)
-    for i in range(args.s+1):
-        thickness = 1
-        if i % sqrt(args.s) == 0:
-            thickness = 4
-        canvas.create_line(10, i*50+10, 50*(args.s)+10, i*50+10,width=thickness )
-        canvas.create_line(i*50+10, 10, i*50+10, 50*(args.s)+10,width=thickness )
-    for y in range(args.s):
-        for x in range(args.s):
-            if grid[y][x] != 0:
-                entry = grid[y][x]
-                if entry > 9:
-                    entry = chr( entry + 55)
-                label = 'entry:{}{}'.format(y,x)
-                canvas.create_text(35+50*x, 35+50*y, font=("Purisa",'30','bold'),
-                text=entry,justify=CENTER, anchor=CENTER,fill='#4d47c4',
-                tag=label)
-    canvas.pack(fill=BOTH, expand=1)
-    return canvas
 
 #Reads in puzzle files and parses them into grid
 #Base code provided by dataset host https://www.kaggle.com/datasets/bryanpark/sudoku?resource=download
@@ -173,26 +99,12 @@ def read_puzzles(filename,size):
 def main():
     global grid
     global total_itrs
-    global animate
     global args
     global last_value
-    global possible_grid
-    generate_possible_grid()
-    window = Tk()
-    canvas = Canvas()
-    if animate:
-        window.title("Sudoku Solver")
-        window.title = "Game"
-        window.geometry("{0}x{0}".format(str(50*(args.s)+20)))
-        canvas = make_base(window)
     start_time = time() #starts timer here to avoid IO distorting data
-    simple_cell_solver(window, canvas) #Attempts to find simple values to reduce time complexity
     last_value = find_last(grid) #Finds last blank, returns true if simple solver filled grid
-    if brute_solver(window, canvas) == True or last_value == True:
-        if animate:
-            sleep(60) #sleeps functions to see solved grid
-        else:
-            print("Runtime: %s seconds" % (time() - start_time))
+    if brute_solver() == True or last_value == True:
+        print("Runtime: %s seconds" % (time() - start_time))
         print("solved in {} interations".format(total_itrs))
         print_grid()
     else:
@@ -207,10 +119,7 @@ if __name__ == "__main__":
     parser.add_argument("-f", "-file", default=False, help="Provide a filename with puzzles")
     parser.add_argument("-s", "-size", default=9,type=int, help="Puzzle size: enter 9 for a regular sudoku")
     args = parser.parse_args()
-    animate = False
     puzzles = []
-    if args.animate:
-        animate = True
     #Base grids for tested matrix sizes
     if args.s == 9:
         grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
@@ -247,10 +156,4 @@ if __name__ == "__main__":
     last_value = None
     total_itrs = 0 #Counts total runs through matrix
     done = False
-    possible_grid = []
-    for i in range(1, args.s+1):
-        row = []
-        for j in range(1, args.s+1):
-            row.append(list(range(1, args.s+1)))
-        possible_grid.append(row)
     main()
